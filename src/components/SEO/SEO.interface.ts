b4fe358272dc5751885b981c8cc42e6f993c485b
matheusdoedev <matheusdoe.dev@gmail.com>
2021-04-07 17:55:54 -0300

    :lipstick: feat: fonts and colors styles added

---
+++ src/components/SEO/SEO.interface.ts
@@ -0,0 +1,6 @@
+export interface SEOProps {
+  lang: string;
+  title: string;
+  type?: string;
+  description: string;
+}
---
+++ src/components/SEO/SEO.tsx
@@ -0,0 +1,29 @@
+import Head from "next/head";
+
+import { siteMetadata } from "../../config/siteMetadata";
+
+import { SEOProps } from "./SEO.interface";
+
+const Seo: React.FC<SEOProps> = ({ title, description, lang }) => {
+  const metaDescription = description || siteMetadata.description;
+
+  return (
+    <html lang={lang}>
+      <Head>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta property="og:type" content={siteMetadata.type} />
+        <meta name="robots" content="index, follow" />
+        <meta property="og:locale" content={lang} />
+        <meta property="og:title" content={title} />
+        <meta property="og:description" content={metaDescription} />
+        <meta name="keywords" content={siteMetadata.keywords} />
+        <meta name="description" content={metaDescription} />
+        <title>
+          {title} - {siteMetadata.title}
+        </title>
+      </Head>
+    </html>
+  );
+};
+
+export default Seo;
