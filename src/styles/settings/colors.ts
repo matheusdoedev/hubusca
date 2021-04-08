8d17f3cc615003fbe486ec5e185b48f44c33d904
matheusdoedev <matheusdoe.dev@gmail.com>
2021-04-08 11:01:23 -0300

    :lipstick: feat: project standard styles added

---
+++ src/styles/settings/colors.ts
@@ -0,0 +1,7 @@
+export const colors = {
+  primary: `#26cad3`,
+  secondary: `#222221`,
+  support: `#26d395`,
+  javascript: `#f1e05a`,
+  gradient: `linear-gradient(180deg, #222221 0%, rgba(34, 34, 33, 0.95) 100%);`,
+};
