fbb66423d7eee6374fa261fc1187862ba4f5407e
matheusdoedev <matheusdoe.dev@gmail.com>
2021-04-11 21:53:00 -0300

    :dizzy: feat: Animations style file added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import "bootstrap/dist/css/bootstrap.css";
 
+import { Animations } from "../styles/settings/animations";
 import { Reset } from "../styles/generic/reset";
 import { Elements } from "../styles/base/elements";
 import { Gaps } from "../styles/tools/gaps";
@@ -9,6 +10,7 @@ import { UserContextComponent } from "../data/contexts/UserContext";
 function MyApp({ Component, pageProps }) {
   return (
     <UserContextComponent>
+      <Animations />
       <Reset />
       <Elements />
       <Gaps />
